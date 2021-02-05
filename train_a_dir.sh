ab4188a0bd7de443f0fdd21a950afd29fc562012
oZwZo <zhengwzh5@hotmail.com>
2021-02-05 21:53:37 +0800

    ` $filename | cut -d -f`  to extract run name

--- train_a_dir.sh
+++ train_a_dir.sh
@@ -2,5 +2,7 @@
 echo "please enter relative directory path"
 read log_dir
 for filename in $log_dir/*.ini; do
-    nohup python script/main_train.py  --config_file $filename > $filename.out &
+    out_name=$(echo $filename | cut -d "." -f 1)
+    
+     nohup python script/main_train.py  --config_file $filename > ${out_name}.out &
 done 
