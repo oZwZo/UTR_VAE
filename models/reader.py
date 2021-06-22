a37bd03083ed5616fca8dc86afdbeae9ef46b587
oZwZo <zhengwzh5@hotmail.com>
2021-06-22 19:39:19 +0800

    reader : reform the `get_splited_dataloader`

--- models/reader.py
+++ models/reader.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import  DataLoader, Dataset ,random_split
+from torch.utils.data import  DataLoader, Dataset ,random_split,IterableDataset
 from sklearn.model_selection import KFold,train_test_split
 import numpy as np
 import pandas as pd
@@ -8,6 +8,7 @@ import os
 import sys
 import json
 from torch import nn
+from bucket_sampler import Bucket_Sampler
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from utils import Seq_one_hot,read_UTR_csv,read_label
 
@@ -41,130 +42,6 @@ def one_hot(seq,complementary=False):
             continue      # for nucleotide that are not in A C G T   
     return oh_array 
 
-class MTL_enc_dataset(Dataset):
-    def __init__(self,DF,pad_to=100,aux_columns=None,input_col=None):
-        """
-        the dataset for Multi-task learning, will return sequence in one-hot encoded version, together with some auxilary task label
-        arguments:
-        ...csv_path: abs path of csv file, column `utr` should be in the csv
-        ...pad_to : maximum length of the sequences
-        ...columns : list  contains what  axuslary task label will be 
-        """
-        self.pad_to = pad_to
-        self.df = DF     # read Df
-        self.seqs = self.df.utr.values       # take out all the sequence in DF
-        self.columns = aux_columns
-        self.input_col = input_col
-                
-    def __len__(self):
-        return self.df.shape[0]
-    
-    def __getitem__(self,index):
-        seq = self.seqs[index]        # sequence: str of len 25~100
-        
-        seq_oh = one_hot(seq)         # seq_oh : np array, one hot encoding sequence 
-        X = self.pad_zeros(seq_oh)    # X  : torch.tensor , zero padded to 100
-        
-        if self.columns == None:
-            # which means no auxilary label is needed
-            item = X,X
-        elif (type(self.columns) == list) & (len(self.columns)!=0):
-            # return what's in columns
-            aux_labels = self.df.loc[:,self.columns].values[index]
-            item = X ,aux_labels
-
-            if self.input_col is not None:
-                input = [X]
-                for col in self.input_col:
-                    input.append(self.df.loc[:,col].values[index]) 
-                item = input,aux_labels
-
-        return item
-        
-    def pad_zeros(self,X):
-        """
-        zero padding at the right end of the sequence
-        """
-        gap = self.pad_to - X.shape[0]
-        pad_fn = nn.ZeroPad2d([0,0,0,gap])  #  (padding_left , padding_right , padding_top , padding_bottom )
-        # gap_array = np.zeros()
-        X_padded = pad_fn(torch.tensor(X))
-        return X_padded
-
-        
-class UTR_dataset(Dataset):
-    def __init__(self,cell_line:str,script_dir = script_dir,data_dir = data_dir):
-        # read csv first
-        self.cell_line = cell_line 
-        self.df = read_UTR_csv(cell_line=cell_line)
-
-        # raw data
-        self.oh_x = Seq_one_hot().d_transform(self.df,flattern=False) # (3970, 100, 4)
-        self.y = self.df.TEaverage.values 
-
-    def __len__(self):
-        return self.oh_x.shape[0]
-    
-    def __getitem__(self,index):
-        return self.oh_x[index],self.y[index]
-
-
-class mask_reader(Dataset):
-    def __init__(self,npy_path):
-        """
-        read the mask A549 sequence and with real sequence
-        """
-        self.data_set = np.load(npy_path)
-        self.X = self.data_set[:,0]
-        self.Y = self.data_set[:,1]
-    
-    def __len__(self):
-        return self.X.shape[0]
-    
-    def __getitem__(self,index):
-        return (self.X[index,:,:],self.Y[index,:,:])
-    
-
-# def get_splited_dataloader(dataset,ratio:list,batch_size,num_workers):
-#     """
-#     split the total dataset into train val test, and return in a DataLoader (train_loader,val_loader,test_loader) 
-#     dataset : the defined <UTR_dataset>
-#     ratio : the ratio of train : val : test
-#     batch_size : int
-#     """
-
-#     # make ratio to length
-#     total_len = len(dataset)
-#     lengths = [int(total_len*ratio[0]),int(len(dataset)*ratio[1])]
-#     lengths.append(total_len-sum(lengths))         # make sure the sum of length is the total len
-
-#     set_ls = random_split(dataset,lengths,generator=torch.Generator().manual_seed(42))         # split dataset 
-    
-
-    # return [DataLoader(subset, batch_size=batch_size, shuffle=True,num_workers=num_workers) for subset in set_ls]
-
-def get_mask_dataloader(batch_size,num_workers):
-    """
-    (train_loader,val_loader,test_loader) 
-    This is a splited data-loader with seed `42` on cell line `A549`
-    each sequence have `5` nucleotide masked
-    """
-    set_ls = ['train','val','test']
-    dataset_ls = [mask_reader(os.path.join(data_dir,'mask_data',set+"_mask.npy")) for set in set_ls]
-    
-    return [DataLoader(subset, batch_size=batch_size, shuffle=True,num_workers=num_workers) for subset in dataset_ls]
-
-def get_mix_dataloader(batch_size,num_workers,shuffle=True):
-    """
-    (train_loader,val_loader,test_loader) 
-    This is a splited data-loader with seed `42` on cell line `A549` and "human-library" and "snv-library"
-    each sequence have `5` nucleotide
-    """
-    set_ls = ['train','val','test']
-    dataset_ls = [mask_reader(os.path.join(data_dir,'mix_data',"mix_%s.npy"%set)) for set in set_ls]
-    
-    return [DataLoader(subset, batch_size=batch_size, shuffle=shuffle,num_workers=num_workers) for subset in dataset_ls]
-
 def read_bpseq(test_bpseq_path):
     """
     read bpseq file, extract sequence and ptable
@@ -190,8 +67,10 @@ def pad_zeros(X,pad_to):
     """
     zero padding at the right end of the sequence
     """
+    if pad_to == 0:
+        pad_to = 8*(X.shape[0]//8 + 1) + 1
     gap = pad_to - X.shape[0]
-
+    
     #  here we change to padding ahead  , previously  nn.ZeroPad2d([0,0,0,gap])
     pad_fn = nn.ZeroPad2d([0,0,gap,0])  #  (padding_left , padding_right , padding_top , padding_bottom )
     # gap_array = np.zeros()
@@ -199,9 +78,42 @@ def pad_zeros(X,pad_to):
     X_padded = pad_fn(X)
     return X_padded
 
-class GSE65778_dataset(Dataset):
+def pack_seq(ds_zls:list):
+    X_ts = [X for X,Y in ds_zls]
+    max_len = np.max([X.shape[0] for X in X_ts])
     
-    def __init__(self,DF,pad_to,trunc_len=50,seq_col='utr',value_col='TE_count',other_input_columns=None):
+    X_packed = torch.stack([pad_zeros(X=x,pad_to=max_len) for x in X_ts])
+    Y_packed = torch.tensor([Y for X,Y in ds_zls])
+    
+    return X_packed , Y_packed
+
+class mask_reader(Dataset):
+    def __init__(self,npy_path):
+        """
+        read the mask A549 sequence and with real sequence
+        """
+        self.data_set = np.load(npy_path)
+        self.X = self.data_set[:,0]
+        self.Y = self.data_set[:,1]
+    
+    def __len__(self):
+        return self.X.shape[0]
+    
+    def __getitem__(self,index):
+        return (self.X[index,:,:],self.Y[index,:,:])
+
+class mix_dataset(Dataset):
+    def __init__(self):
+        # dataset_ls = [mask_reader(os.path.join(data_dir,'mix_data',"mix_%s.npy"%set)) for set in ['train','val','test']]
+        raise ValueError("the dataset is not defined")
+
+class mask_dataset(Dataset):
+    def __init__(self):
+        raise ValueError("the dataset is not defined")
+
+class ribo_dataset(Dataset):
+    
+    def __init__(self,DF,pad_to,trunc_len=50,seq_col='utr',aux_columns='TE_count',other_input_columns=None):
         """
         Dataset to trancate sequence and return in one-hot encoding way
         `dataset(DF,pad_to,trunc_len=50,seq_col='utr')`
@@ -217,7 +129,7 @@ class GSE65778_dataset(Dataset):
         # X and Y
         self.seqs = self.df.loc[:,seq_col].values
         self.other_input_columns = other_input_columns
-        self.Y = self.df.loc[:,value_col].values
+        self.Y = self.df.loc[:,aux_columns].values
         
     def __len__(self):
         return self.df.shape[0]
@@ -237,7 +149,7 @@ class GSE65778_dataset(Dataset):
         """
         truncate the sequence and encode in one hot
         """
-        if len(seq) >  self.trunc_len:
+        if (len(seq) >  self.trunc_len)&(self.trunc_len >0):
             seq = seq[-1* self.trunc_len:]
         
         X = one_hot(seq)
@@ -247,8 +159,50 @@ class GSE65778_dataset(Dataset):
         
         return X_padded.float()
 
+class MTL_dataset(Dataset):
+    def __init__(self,DF,pad_to=100,seq_col='utr',aux_columns=None,other_input_columns=None,trunc_len=None):
+        """
+        the dataset for Multi-task learning, will return sequence in one-hot encoded version, together with some auxilary task label
+        arguments:
+        ...csv_path: abs path of csv file, column `utr` should be in the csv
+        ...pad_to : maximum length of the sequences
+        ...columns : list  contains what  axuslary task label will be 
+        """
+        self.pad_to = pad_to
+        self.trunc_len = trunc_len
+        self.df = DF     # read Df
+        self.seqs = self.df[seq_col].values       # take out all the sequence in DF
+        self.columns = aux_columns
+        self.other_input_columns = other_input_columns
+        
+        assert trunc_len == None, "MTL dataset does not support argument trunc len"
+                
+    def __len__(self):
+        return self.df.shape[0]
+    
+    def __getitem__(self,index):
+        seq = self.seqs[index]        # sequence: str of len 25~100
+        
+        X = one_hot(seq)         # seq_oh : np array, one hot encoding sequence 
+        # X = self.pad_zeros(X)    # X  : torch.tensor , zero padded to 100
+        
+        if self.columns == None:
+            # which means no auxilary label is needed
+            item = X,X
+        elif (type(self.columns) == list) & (len(self.columns)!=0):
+            # return what's in columns
+            aux_labels = self.df.loc[:,self.columns].values[index]
+            item = X ,aux_labels
 
-def get_splited_dataloader(dataset_func,df_ls,ratio:list,batch_size,num_workers,seed=42,return_dataset=False):
+            if self.other_input_columns is not None:
+                input = [X]
+                for col in self.other_input_columns:
+                    input.append(self.df.loc[:,col].values[index]) 
+                item = input,aux_labels
+
+        return item   
+
+def get_splited_dataloader(dataset_func, df_ls, ratio:list, batch_size, shuffle, pad_to, seed=42,return_dataset=False):
     """
     split the total dataset into train val test, and return in a DataLoader (train_loader,val_loader,test_loader) 
     dataset : the defined <UTR_dataset>
@@ -257,85 +211,51 @@ def get_splited_dataloader(dataset_func,df_ls,ratio:list,batch_size,num_workers,
     """
 
     #  determined set-ls
-    if len(df_ls) == 1:
-        dataset = dataset_func(df_ls[0])
-        
-        total_len = len(dataset)
-        lengths = [int(total_len*sub_ratio) for sub_ratio in ratio[:-1]]
-        lengths.append(total_len-sum(lengths))         # make sure the sum of length is the total len
-
-        set_ls = random_split(dataset,lengths,generator=torch.Generator().manual_seed(seed))         # split dataset 
     
+    set_ls = [dataset_func(df) for df in df_ls]
+    if return_dataset:
+        return set_ls
+    
+    if pad_to == 0 :
+        # automatic padding to `8x -1`
+        # and we will pad the sequence of similar length with bucket sampler
+        sampler_ls = [{"batch_sampler":Bucket_Sampler(df,batch_size=batch_size,drop_last=False),
+                       "num_workers":4,
+                       "collate_fn":pack_seq} for df in df_ls]
+        #  wrap dataset to dataloader
+        loader_ls = [DataLoader(subset,**kwargs) for subset,kwargs in zip(set_ls,sampler_ls)]
     else:
-        set_ls = [dataset_func(df) for df in df_ls]
-    
-    #  wrap dataset to dataloader
-    loader_ls = [
-                 DataLoader(subset,batch_size=batch_size,
-                            shuffle=True,num_workers=num_workers,
-                            generator=torch.Generator().manual_seed(42)) for subset in set_ls
-                 ]
+        loaderargs = {"batch_size": batch_size,
+                        "generator":torch.Generator().manual_seed(42),
+                        "num_workers":4,
+                        "shuffle":shuffle}
+        #  wrap dataset to dataloader
+        loader_ls = [DataLoader(subset,**loaderargs) for subset in set_ls]
+        
     if len(loader_ls) == 2:
         # a complement of empty test set
         loader_ls.append(None) 
-    if return_dataset:
-        return set_ls
+        
+    return loader_ls
+
+def split_DF(csv_path,ratio,kfold_cv,kfold_index=None,seed=42):
+    
+    full_df = pd.read_csv(csv_path)
+        
+    if kfold_cv == True:
+        # K-fold CV : 8:1:1 for each partition
+        df_ls = KFold_df_split(full_df,kfold_index)
     else:
-        return loader_ls
+        # POPEN.ratio will determine train :val :test ratio
+        total_len = len(full_df)
+        lengths = [int(total_len*sub_ratio) for sub_ratio in ratio[:-1]]
+        lengths.append(total_len-sum(lengths))         # make sure the sum of length is the total len
 
-
-
-def get_dataloader(POPEN):
-    """
-    wrapper
-    """
-    if POPEN.dataset == 'mix':
-        loader_ls  = get_mix_dataloader(batch_size=POPEN.batch_size,num_workers=4)
-    elif POPEN.dataset == "mask":
-        loader_ls = get_mask_dataloader(batch_size=POPEN.batch_size,num_workers=4)
-        
-    elif POPEN.dataset == "ribo":
-        full_df = pd.read_csv(POPEN.csv_path)
-        
-        if POPEN.kfold_cv == True:
-            # K-fold CV : 8:1:1 for each partition
-            df_ls = KFold_df_split(full_df,POPEN.kfold_index)
-        else:
-            # POPEN.ratio will determine train :val :test ratio
-            df_ls = [full_df] 
-        
-        dataset_func = lambda x :GSE65778_dataset(x,pad_to=POPEN.pad_to,trunc_len=POPEN.trunc_len,
-                                                    seq_col=POPEN.seq_col,value_col=POPEN.aux_task_columns,
-                                                    other_input_columns=POPEN.other_input_columns)
+        set_ls = random_split(full_df,lengths,generator=torch.Generator().manual_seed(seed)) 
+        df_ls = [full_df.iloc[subset.indices] for subset in set_ls] # df.iloc [ idx ]
         
             
-        loader_ls = get_splited_dataloader(dataset_func,df_ls,ratio=POPEN.train_test_ratio,
-                                            batch_size=POPEN.batch_size,num_workers=4,seed=42) # new function
-    elif POPEN.dataset == "MTL":
-        full_df = pd.read_csv(POPEN.csv_path)        
-        
-        if type(POPEN.split_like_paper) == list:
-            # two csv path : 260,000 train set & 20,000 test set
-            df_ls = [pd.read_csv(path) for path in POPEN.split_like_paper]
-        elif POPEN.kfold_cv == True:
-            # K-fold CV : 8:1:1 for each partition
-            df_ls = KFold_df_split(full_df,POPEN.kfold_index)
-        else:
-            df_ls = [full_df]
-        
-        dataset_func = lambda x : MTL_enc_dataset(DF=x,pad_to=POPEN.pad_to,
-                                        aux_columns=POPEN.aux_task_columns,input_col=POPEN.other_input_columns)
-        
-        loader_ls = get_splited_dataloader(dataset_func,df_ls,ratio=POPEN.train_test_ratio,
-                                            batch_size=POPEN.batch_size,num_workers=4,seed=42) # new function
-        
-    else:
-        # YK 's dataset
-        dataset = UTR_dataset(cell_line=POPEN.cell_line)
-        loader_ls = get_splited_dataloader(dataset,ratio=[0.7,0.1,0.2],
-                                            batch_size=POPEN.batch_size,num_workers=4,seed=42)
-         # train,  val , test
-    return loader_ls 
+    return df_ls
 
 def KFold_df_split(df,K,**kfoldargs):
     """
@@ -358,46 +278,25 @@ def KFold_df_split(df,K,**kfoldargs):
     # the remaining 1/5 data will further break into val and test
     val_df,test_df = train_test_split(val_test_df,test_size=0.5,random_state=42)
     
-    return [train_df,val_df,test_df]
+    return [train_df,val_df,test_df]   
+    
 
-def DF_to_dataloader(full_df,POPEN,return_dataset):
+def get_dataloader(POPEN):
     """
-    An function that convert any dataframe into MTL_enc_dataset and its dataloader
-    Args:
-        full_df : list , [DataFrames,] ,must contain : ['utr','rl','scaled_rl','with_xxx'..]
-        POPEN :
-        return_dataset : boolen, if True, will return dataset instead of dataloader
-    return 
-        loader_ls : list , [torch.utils.data.DataLoader,] or [torch.utils.data.DataSet,] depending on `return dataset`
-    """
-    df_ls = [full_df]
-
-    dataset_func = lambda x : MTL_enc_dataset(DF=x,pad_to=POPEN.pad_to,
-                                aux_columns=POPEN.aux_task_columns,input_col=POPEN.other_input_columns)
-
-    loader_ls = get_splited_dataloader(dataset_func,df_ls,ratio=POPEN.train_test_ratio,
-                                    batch_size=POPEN.batch_size,num_workers=4,seed=42,return_dataset=return_dataset) #
-    return loader_ls
-
-
-def loader_from_df(df,POPEN,return_set=False):
-    """
-    unlike `reader.DF_to_dataloader`, the dataloader from this function has no train test split, it used entire df to return data
-    Args:
-        df: DataFrame , must contain : ['utr','rl','scaled_rl','with_xxx'..]
-        POPEN : `models.popen.Auto_popen`
-    Return:
-        dataloader
+    wrapper
     """
     
-    dataset_func = lambda x : MTL_enc_dataset(DF=x,pad_to=POPEN.pad_to,
-                                aux_columns=POPEN.aux_task_columns,input_col=POPEN.other_input_columns)
+    df_ls = split_DF(POPEN.csv_path,POPEN.train_test_ratio,POPEN.kfold_cv,POPEN.kfold_index,seed=42)
     
-    subset = dataset_func(df)
-    dataloader = DataLoader(subset,batch_size=126,
-                            shuffle=False,num_workers=2,
-                            generator=torch.Generator().manual_seed(42))
-    if return_set:
-        return subset
-    else:
-        return dataloader
+    
+    DS_Class = eval(POPEN.dataset+"_dataset")
+         
+    dataset_func = lambda x : DS_Class(x,pad_to=POPEN.pad_to,trunc_len=POPEN.trunc_len,
+                                         seq_col=POPEN.seq_col, aux_columns=POPEN.aux_task_columns,
+                                         other_input_columns=POPEN.other_input_columns)
+    
+    loader_ls = get_splited_dataloader(dataset_func, df_ls,ratio=POPEN.train_test_ratio,
+                                        batch_size=POPEN.batch_size, shuffle=True,
+                                        pad_to=POPEN.pad_to, seed=42) # new function
+        
+    return loader_ls 
